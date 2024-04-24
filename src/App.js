b3c02626d8f901a35fb28c6b9b70b25fdec7764b
Tanishka khamesara <tanishkajain24941@gmail.com>
2024-04-24 11:48:55 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useRef, useState } from 'react';
 function App() {
+  const [inputText, setInputText] = useState();
+  const [content, setContent] = useState([]);
+  async function fetchedData() {
+    const URL=`https://api.unsplash.com/search/photos?page=1&query=${inputText}&client_id=qQu6cTrG58GZm1N_6OBgFrFhjTRoGJXFzbDaOnKIexw`
+    const response = await fetch(URL);
+    const data = await response.json();
+    const details = data.results;
+    console.log(details);
+    setContent(details);
+   setInputText("")
+  }
+  
+  // console.log(content);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hello
-        </a>
-      </header>
+      <div>
+        <h2>Image Generation App</h2>
+        <input type="text" placeholder="Enter Text..." value={inputText} onChange={(e) => {
+          setInputText(e.target.value);
+          
+        }} />
+        <button onClick={fetchedData} >Generate</button>
+      </div>
+      <div style={{margin:"20px",display:"flex",flexWrap:"wrap",gap:"4px"}}>
+        {
+          content.map((ele,index) => {
+            return (
+              <div key={index}>
+                <img src={ele.urls.
+small_s3
+} style={{height:"400px",width:"400px"}} />
+
+              </div>
+             
+           )
+          })
+        }
+      </div>
+      
     </div>
   );
 }
